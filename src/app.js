40b3ebb0ee6d1947b04b70f8da64408ce3bf7236
Lor <lor.develop@gmail.com>
2021-03-05 19:17:47 +0100

    Show current time and temperature

---
+++ src/app.js
@@ -0,0 +1,47 @@
+function formatDate(time) {
+  let date = new Date(time);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function showTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.dt);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let realFeelElement = document.querySelector("#realfeel");
+  realFeelElement.innerHTML = Math.round(response.data.main.feels_like);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt);
+}
+
+let apiKey = "cf76ec6bfd9d3c2ea4f7240c87482c39";
+let city = "Burgos";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
