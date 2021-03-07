83a6fd9c83366c3ef24e29415319fdd40cabae53
Lor <lor.develop@gmail.com>
2021-03-07 12:01:07 +0100

    Improved code for the forecast

--- src/app.js
+++ src/app.js
@@ -24,136 +24,136 @@ function formatDate(time) {
 
 // Forecast days
 
-let currentDate = new Date();
+// let currentDate = new Date();
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+// let days = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
 
-let secondDay = document.querySelector(".second-weather");
-let secondWeather = currentDate.getDay() + 2;
-let thirdDay = document.querySelector(".third-weather");
-let thirdWeather = currentDate.getDay() + 3;
+// let secondDay = document.querySelector(".second-weather");
+// let secondWeather = currentDate.getDay() + 2;
+// let thirdDay = document.querySelector(".third-weather");
+// let thirdWeather = currentDate.getDay() + 3;
 
-let fourthDay = document.querySelector(".fourth-weather");
-let fourthWeather = currentDate.getDay() + 4;
+// let fourthDay = document.querySelector(".fourth-weather");
+// let fourthWeather = currentDate.getDay() + 4;
 
-let fifthDay = document.querySelector(".fifth-weather");
-let fifthWeather = currentDate.getDay() + 5;
+// let fifthDay = document.querySelector(".fifth-weather");
+// let fifthWeather = currentDate.getDay() + 5;
 
-if (currentDate.getDay() === 2) {
-  secondDay.innerHTML = days[secondWeather];
-  thirdDay.innerHTML = days[thirdWeather];
-  fourthDay.innerHTML = days[fourthWeather];
-  fifthDay.innerHTML = days[0];
-} else {
-  if (currentDate.getDay() === 3) {
-    secondDay.innerHTML = days[secondWeather];
-    thirdDay.innerHTML = days[thirdWeather];
-    fourthDay.innerHTML = days[0];
-    fifthDay.innerHTML = days[1];
-  } else {
-    if (currentDate.getDay() === 4) {
-      secondDay.innerHTML = days[secondWeather];
-      thirdDay.innerHTML = days[0];
-      fourthDay.innerHTML = days[1];
-      fifthDay.innerHTML = days[2];
-    } else {
-      if (currentDate.getDay() === 5) {
-        secondDay.innerHTML = days[0];
-        thirdDay.innerHTML = days[1];
-        fourthDay.innerHTML = days[2];
-        fifthDay.innerHTML = days[3];
-      } else {
-        if (currentDate.getDay() === 6) {
-          secondDay.innerHTML = days[1];
-          thirdDay.innerHTML = days[2];
-          fourthDay.innerHTML = days[3];
-          fifthDay.innerHTML = days[4];
-        } else {
-          secondDay.innerHTML = days[secondWeather];
-          thirdDay.innerHTML = days[thirdWeather];
-          fourthDay.innerHTML = days[fourthWeather];
-          fifthDay.innerHTML = days[fifthWeather];
-        }
-      }
-    }
-  }
-}
-let maxTemperature1 = null;
-let minTemperature1 = null;
-let maxTemperature2 = null;
-let minTemperature2 = null;
-let maxTemperature3 = null;
-let minTemperature3 = null;
-let maxTemperature4 = null;
-let minTemperature4 = null;
-let maxTemperature5 = null;
-let minTemperature5 = null;
-function showForecast(response) {
-  console.log(response.data);
-  let maxTemp1 = document.querySelector(".max-temp1");
-  let minTemp1 = document.querySelector(".min-temp1");
-  let maxTemp2 = document.querySelector(".max-temp2");
-  let minTemp2 = document.querySelector(".min-temp2");
-  let maxTemp3 = document.querySelector(".max-temp3");
-  let minTemp3 = document.querySelector(".min-temp3");
-  let maxTemp4 = document.querySelector(".max-temp4");
-  let minTemp4 = document.querySelector(".min-temp4");
-  let maxTemp5 = document.querySelector(".max-temp5");
-  let minTemp5 = document.querySelector(".min-temp5");
-  maxTemperature1 = Math.round(response.data.daily[1].temp.max);
-  minTemperature1 = Math.round(response.data.daily[1].temp.min);
-  maxTemperature2 = Math.round(response.data.daily[2].temp.max);
-  minTemperature2 = Math.round(response.data.daily[2].temp.min);
-  maxTemperature3 = Math.round(response.data.daily[3].temp.max);
-  minTemperature3 = Math.round(response.data.daily[3].temp.min);
-  maxTemperature4 = Math.round(response.data.daily[4].temp.max);
-  minTemperature4 = Math.round(response.data.daily[4].temp.min);
-  maxTemperature5 = Math.round(response.data.daily[5].temp.max);
-  minTemperature5 = Math.round(response.data.daily[5].temp.min);
-  maxTemp1.innerHTML = maxTemperature1;
-  minTemp1.innerHTML = minTemperature1;
-  maxTemp2.innerHTML = maxTemperature2;
-  minTemp2.innerHTML = minTemperature2;
-  maxTemp3.innerHTML = maxTemperature3;
-  minTemp3.innerHTML = minTemperature3;
-  maxTemp4.innerHTML = maxTemperature4;
-  minTemp4.innerHTML = minTemperature4;
-  maxTemp5.innerHTML = maxTemperature5;
-  minTemp5.innerHTML = minTemperature5;
-  let iconElement1 = document.querySelector("#icon1");
-  iconElement1.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.daily[1].weather[0].icon}@2x.png`
-  );
-  let iconElement2 = document.querySelector("#icon2");
-  iconElement2.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.daily[2].weather[0].icon}@2x.png`
-  );
-  let iconElement3 = document.querySelector("#icon3");
-  iconElement3.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.daily[3].weather[0].icon}@2x.png`
-  );
-  let iconElement4 = document.querySelector("#icon4");
-  iconElement4.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.daily[4].weather[0].icon}@2x.png`
-  );
-  let iconElement5 = document.querySelector("#icon5");
-  iconElement5.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.daily[5].weather[0].icon}@2x.png`
-  );
-}
+// if (currentDate.getDay() === 2) {
+//   secondDay.innerHTML = days[secondWeather];
+//   thirdDay.innerHTML = days[thirdWeather];
+//   fourthDay.innerHTML = days[fourthWeather];
+//   fifthDay.innerHTML = days[0];
+// } else {
+//   if (currentDate.getDay() === 3) {
+//     secondDay.innerHTML = days[secondWeather];
+//     thirdDay.innerHTML = days[thirdWeather];
+//     fourthDay.innerHTML = days[0];
+//     fifthDay.innerHTML = days[1];
+//   } else {
+//     if (currentDate.getDay() === 4) {
+//       secondDay.innerHTML = days[secondWeather];
+//       thirdDay.innerHTML = days[0];
+//       fourthDay.innerHTML = days[1];
+//       fifthDay.innerHTML = days[2];
+//     } else {
+//       if (currentDate.getDay() === 5) {
+//         secondDay.innerHTML = days[0];
+//         thirdDay.innerHTML = days[1];
+//         fourthDay.innerHTML = days[2];
+//         fifthDay.innerHTML = days[3];
+//       } else {
+//         if (currentDate.getDay() === 6) {
+//           secondDay.innerHTML = days[1];
+//           thirdDay.innerHTML = days[2];
+//           fourthDay.innerHTML = days[3];
+//           fifthDay.innerHTML = days[4];
+//         } else {
+//           secondDay.innerHTML = days[secondWeather];
+//           thirdDay.innerHTML = days[thirdWeather];
+//           fourthDay.innerHTML = days[fourthWeather];
+//           fifthDay.innerHTML = days[fifthWeather];
+//         }
+//       }
+//     }
+//   }
+// }
+// let maxTemperature1 = null;
+// let minTemperature1 = null;
+// let maxTemperature2 = null;
+// let minTemperature2 = null;
+// let maxTemperature3 = null;
+// let minTemperature3 = null;
+// let maxTemperature4 = null;
+// let minTemperature4 = null;
+// let maxTemperature5 = null;
+// let minTemperature5 = null;
+// function showForecast(response) {
+//   console.log(response.data);
+//   let maxTemp1 = document.querySelector(".max-temp1");
+//   let minTemp1 = document.querySelector(".min-temp1");
+//   let maxTemp2 = document.querySelector(".max-temp2");
+//   let minTemp2 = document.querySelector(".min-temp2");
+//   let maxTemp3 = document.querySelector(".max-temp3");
+//   let minTemp3 = document.querySelector(".min-temp3");
+//   let maxTemp4 = document.querySelector(".max-temp4");
+//   let minTemp4 = document.querySelector(".min-temp4");
+//   let maxTemp5 = document.querySelector(".max-temp5");
+//   let minTemp5 = document.querySelector(".min-temp5");
+//   maxTemperature1 = Math.round(response.data.daily[1].temp.max);
+//   minTemperature1 = Math.round(response.data.daily[1].temp.min);
+//   maxTemperature2 = Math.round(response.data.daily[2].temp.max);
+//   minTemperature2 = Math.round(response.data.daily[2].temp.min);
+//   maxTemperature3 = Math.round(response.data.daily[3].temp.max);
+//   minTemperature3 = Math.round(response.data.daily[3].temp.min);
+//   maxTemperature4 = Math.round(response.data.daily[4].temp.max);
+//   minTemperature4 = Math.round(response.data.daily[4].temp.min);
+//   maxTemperature5 = Math.round(response.data.daily[5].temp.max);
+//   minTemperature5 = Math.round(response.data.daily[5].temp.min);
+//   maxTemp1.innerHTML = maxTemperature1;
+//   minTemp1.innerHTML = minTemperature1;
+//   maxTemp2.innerHTML = maxTemperature2;
+//   minTemp2.innerHTML = minTemperature2;
+//   maxTemp3.innerHTML = maxTemperature3;
+//   minTemp3.innerHTML = minTemperature3;
+//   maxTemp4.innerHTML = maxTemperature4;
+//   minTemp4.innerHTML = minTemperature4;
+//   maxTemp5.innerHTML = maxTemperature5;
+//   minTemp5.innerHTML = minTemperature5;
+//   let iconElement1 = document.querySelector("#icon1");
+//   iconElement1.setAttribute(
+//     "src",
+//     `https://openweathermap.org/img/wn/${response.data.daily[1].weather[0].icon}@2x.png`
+//   );
+//   let iconElement2 = document.querySelector("#icon2");
+//   iconElement2.setAttribute(
+//     "src",
+//     `https://openweathermap.org/img/wn/${response.data.daily[2].weather[0].icon}@2x.png`
+//   );
+//   let iconElement3 = document.querySelector("#icon3");
+//   iconElement3.setAttribute(
+//     "src",
+//     `https://openweathermap.org/img/wn/${response.data.daily[3].weather[0].icon}@2x.png`
+//   );
+//   let iconElement4 = document.querySelector("#icon4");
+//   iconElement4.setAttribute(
+//     "src",
+//     `https://openweathermap.org/img/wn/${response.data.daily[4].weather[0].icon}@2x.png`
+//   );
+//   let iconElement5 = document.querySelector("#icon5");
+//   iconElement5.setAttribute(
+//     "src",
+//     `https://openweathermap.org/img/wn/${response.data.daily[5].weather[0].icon}@2x.png`
+//   );
+// }
 
 function showTemperature(response) {
   console.log(response.data);
@@ -218,36 +218,36 @@ function showFahrenheitTemperature(event) {
   let fahrenheitTemperature = (celsius * 9) / 5 + 32;
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(fahrenheitTemperature);
-  let fahrenheitTemperatureMax1 = (maxTemperature1 * 9) / 5 + 32;
-  let fahrenheitTemperatureMin1 = (minTemperature1 * 9) / 5 + 32;
-  let fahrenheitTemperatureMax2 = (maxTemperature2 * 9) / 5 + 32;
-  let fahrenheitTemperatureMin2 = (minTemperature2 * 9) / 5 + 32;
-  let fahrenheitTemperatureMax3 = (maxTemperature3 * 9) / 5 + 32;
-  let fahrenheitTemperatureMin3 = (minTemperature3 * 9) / 5 + 32;
-  let fahrenheitTemperatureMax4 = (maxTemperature4 * 9) / 5 + 32;
-  let fahrenheitTemperatureMin4 = (minTemperature4 * 9) / 5 + 32;
-  let fahrenheitTemperatureMax5 = (maxTemperature5 * 9) / 5 + 32;
-  let fahrenheitTemperatureMin5 = (minTemperature5 * 9) / 5 + 32;
-  let maxTemp1 = document.querySelector(".max-temp1");
-  let minTemp1 = document.querySelector(".min-temp1");
-  let maxTemp2 = document.querySelector(".max-temp2");
-  let minTemp2 = document.querySelector(".min-temp2");
-  let maxTemp3 = document.querySelector(".max-temp3");
-  let minTemp3 = document.querySelector(".min-temp3");
-  let maxTemp4 = document.querySelector(".max-temp4");
-  let minTemp4 = document.querySelector(".min-temp4");
-  let maxTemp5 = document.querySelector(".max-temp5");
-  let minTemp5 = document.querySelector(".min-temp5");
-  maxTemp1.innerHTML = Math.round(fahrenheitTemperatureMax1);
-  minTemp1.innerHTML = Math.round(fahrenheitTemperatureMin1);
-  maxTemp2.innerHTML = Math.round(fahrenheitTemperatureMax2);
-  minTemp2.innerHTML = Math.round(fahrenheitTemperatureMin2);
-  maxTemp3.innerHTML = Math.round(fahrenheitTemperatureMax3);
-  minTemp3.innerHTML = Math.round(fahrenheitTemperatureMin3);
-  maxTemp4.innerHTML = Math.round(fahrenheitTemperatureMax4);
-  minTemp4.innerHTML = Math.round(fahrenheitTemperatureMin4);
-  maxTemp5.innerHTML = Math.round(fahrenheitTemperatureMax5);
-  minTemp5.innerHTML = Math.round(fahrenheitTemperatureMin5);
+  //   let fahrenheitTemperatureMax1 = (maxTemperature1 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMin1 = (minTemperature1 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMax2 = (maxTemperature2 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMin2 = (minTemperature2 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMax3 = (maxTemperature3 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMin3 = (minTemperature3 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMax4 = (maxTemperature4 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMin4 = (minTemperature4 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMax5 = (maxTemperature5 * 9) / 5 + 32;
+  //   let fahrenheitTemperatureMin5 = (minTemperature5 * 9) / 5 + 32;
+  //   let maxTemp1 = document.querySelector(".max-temp1");
+  //   let minTemp1 = document.querySelector(".min-temp1");
+  //   let maxTemp2 = document.querySelector(".max-temp2");
+  //   let minTemp2 = document.querySelector(".min-temp2");
+  //   let maxTemp3 = document.querySelector(".max-temp3");
+  //   let minTemp3 = document.querySelector(".min-temp3");
+  //   let maxTemp4 = document.querySelector(".max-temp4");
+  //   let minTemp4 = document.querySelector(".min-temp4");
+  //   let maxTemp5 = document.querySelector(".max-temp5");
+  //   let minTemp5 = document.querySelector(".min-temp5");
+  //   maxTemp1.innerHTML = Math.round(fahrenheitTemperatureMax1);
+  //   minTemp1.innerHTML = Math.round(fahrenheitTemperatureMin1);
+  //   maxTemp2.innerHTML = Math.round(fahrenheitTemperatureMax2);
+  //   minTemp2.innerHTML = Math.round(fahrenheitTemperatureMin2);
+  //   maxTemp3.innerHTML = Math.round(fahrenheitTemperatureMax3);
+  //   minTemp3.innerHTML = Math.round(fahrenheitTemperatureMin3);
+  //   maxTemp4.innerHTML = Math.round(fahrenheitTemperatureMax4);
+  //   minTemp4.innerHTML = Math.round(fahrenheitTemperatureMin4);
+  //   maxTemp5.innerHTML = Math.round(fahrenheitTemperatureMax5);
+  //   minTemp5.innerHTML = Math.round(fahrenheitTemperatureMin5);
   celsiusLink.classList.remove("activation");
   fahrenheitLink.classList.add("activation");
 }
@@ -256,30 +256,75 @@ function showCelsiusTemperature(event) {
   event.preventDefault();
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = celsius;
-  let maxTemp1 = document.querySelector(".max-temp1");
-  let minTemp1 = document.querySelector(".min-temp1");
-  let maxTemp2 = document.querySelector(".max-temp2");
-  let minTemp2 = document.querySelector(".min-temp2");
-  let maxTemp3 = document.querySelector(".max-temp3");
-  let minTemp3 = document.querySelector(".min-temp3");
-  let maxTemp4 = document.querySelector(".max-temp4");
-  let minTemp4 = document.querySelector(".min-temp4");
-  let maxTemp5 = document.querySelector(".max-temp5");
-  let minTemp5 = document.querySelector(".min-temp5");
-  maxTemp1.innerHTML = maxTemperature1;
-  minTemp1.innerHTML = minTemperature1;
-  maxTemp2.innerHTML = maxTemperature2;
-  minTemp2.innerHTML = minTemperature2;
-  maxTemp3.innerHTML = maxTemperature3;
-  minTemp3.innerHTML = minTemperature3;
-  maxTemp4.innerHTML = maxTemperature4;
-  minTemp4.innerHTML = minTemperature4;
-  maxTemp5.innerHTML = maxTemperature5;
-  minTemp5.innerHTML = minTemperature5;
+  //   let maxTemp1 = document.querySelector(".max-temp1");
+  //   let minTemp1 = document.querySelector(".min-temp1");
+  //   let maxTemp2 = document.querySelector(".max-temp2");
+  //   let minTemp2 = document.querySelector(".min-temp2");
+  //   let maxTemp3 = document.querySelector(".max-temp3");
+  //   let minTemp3 = document.querySelector(".min-temp3");
+  //   let maxTemp4 = document.querySelector(".max-temp4");
+  //   let minTemp4 = document.querySelector(".min-temp4");
+  //   let maxTemp5 = document.querySelector(".max-temp5");
+  //   let minTemp5 = document.querySelector(".min-temp5");
+  //   maxTemp1.innerHTML = maxTemperature1;
+  //   minTemp1.innerHTML = minTemperature1;
+  //   maxTemp2.innerHTML = maxTemperature2;
+  //   minTemp2.innerHTML = minTemperature2;
+  //   maxTemp3.innerHTML = maxTemperature3;
+  //   minTemp3.innerHTML = minTemperature3;
+  //   maxTemp4.innerHTML = maxTemperature4;
+  //   minTemp4.innerHTML = minTemperature4;
+  //   maxTemp5.innerHTML = maxTemperature5;
+  //   minTemp5.innerHTML = minTemperature5;
   celsiusLink.classList.add("activation");
   fahrenheitLink.classList.remove("activation");
 }
 
+//Forecast
+function formatDay(time) {
+  let date = new Date(time);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[date.getDay()];
+}
+
+function showForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+
+  for (let i = 0; i < 5; i++) {
+    let forecast = response.data.daily[i];
+    forecastElement.innerHTML += `<div class="card border mx-1 mb-2 forecast-card">
+              <div class="card-body">
+                <h5 class="card-title text-center forecast">${formatDay(
+                  forecast.dt * 1000
+                )}</h5>
+                <img src="https://openweathermap.org/img/wn/${
+                  forecast.weather[0].icon
+                }@2x.png" alt="${forecast.weather[0].description}" id="icon1" />
+                <p class="card-text text-center">
+                  <span class="max-temp1"><strong>${Math.round(
+                    forecast.temp.max
+                  )}</strong></span>º/<span
+                    class="min-temp1"
+                  >${Math.round(forecast.temp.min)}</span
+                  >º
+                </p>
+              </div>
+            </div>
+            </div>`;
+  }
+}
+
+//List of cities on the top
 function showMadridTemperature(event) {
   event.preventDefault();
   let madridElement = document.querySelector(".madrid");
